c761f9e2a302c0e3ceb265dbdca04f3aebb812db
mdar4 <darasilva2391@gmail.com>
2021-06-26 21:07:43 -0300

    Documentando o código atual

--- ProjetoDados.py
+++ ProjetoDados.py
@@ -15,7 +15,7 @@ from time import sleep #Lib para realizar temporizar tarefas
 import random #Lib para realizar sorteio de números aleatórios
 from operator import itemgetter # lib para organizar items da list()
 
-vit1 = vit2 = vit3 = vit4 = empates = 0 
+vit1 = vit2 = vit3 = vit4 = empates = 0  # Declarando vitórias e empates
 
 print('''
     VAMOS JOGAR DADOS ?
@@ -32,6 +32,7 @@ sleep(2)
 rod = int(input('Número de rodadas: ')) # Declarando o número de rodadas.
 print()
 clear()
+# Cada Jogador terá um valor sorteado através da lib Random
 for c in range(rod):
     jogo = {'Player1': random.randint(1,6),
             'Player2': random.randint(1,6),
@@ -42,6 +43,7 @@ for c in range(rod):
     for k, v in jogo.items():
      print(f'{k} tirou {v} no dado.')
     sleep(1)
+    # Validando o vencedor de cada Rodada
     if (jogo['Player3'] < jogo['Player1'] > jogo['Player2']) and jogo['Player1'] > jogo['Player4'] :
         vit1 +=1
     elif (jogo['Player3'] < jogo['Player2'] > jogo['Player1']) and jogo['Player2'] > jogo['Player4'] :
@@ -56,15 +58,16 @@ for c in range(rod):
 
     print('-*' * 40)
     print('   ==== Ranking ===')
-    for i, v in enumerate(placar):
+    for i, v in enumerate(placar): 
         print(f'{i+1}° lugar: {v[0]} com {v[1]}.') 
         sleep(1)
     print()
     print('*' * 40)
     clear()
-    
+
 print('E O GRANDE VENCEDOR FOI ...')
 sleep(2)
+# Validando o vencedor
 if vit3 < vit1 > vit2 and vit1 > vit4:
   print(f'O Player 1 venceu com {vit1} vitórias.')
 elif vit3 < vit2 > vit1 and vit2 > vit4:
@@ -74,6 +77,7 @@ elif vit2 < vit3 > vit1 and vit3 > vit4:
 elif vit3 < vit4 > vit1 and vit4 > vit2:
   print(f'O Player 4 venceu com {vit4} vitórias.')
 print()
+# Validando o número de vitórias de cada Jogador
 print(f'''
 O Player 1 teve {vit1} vitórias.
 O Player 2 teve {vit2} vitórias.
